fbe73d397ccaef0d8158fcf6f16462e0223b2a0f
talentamaulana10 <tatamf.id@gmail.com>
2019-04-01 21:21:30 +0700

    passing data to component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    count: 0
+    count: this.props.value
   };
 
   handleIncrement = Product => {
@@ -14,6 +14,7 @@ export default class Counter extends Component {
   };
 
   render() {
+    console.log("props", this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
