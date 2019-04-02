a177ac92ccaf188cb43bf6b06bd4ac055ab044da
talentamaulana10 <tatamf.id@gmail.com>
2019-04-02 10:12:53 +0700

    Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,9 +14,22 @@ class Counters extends Component {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
   };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             onDelete={this.handleDelete}
